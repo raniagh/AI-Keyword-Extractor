f6b3674182e67c58bf22cf16ff5a4ca61e6ab4df
Your Name <rghzaiel@ggn-group.com>
2023-07-11 15:24:49 +0100

    header & footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Box, Container } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
       <Container maxW="3xl" centerContent>
-        App
+        <Header />
+        <Footer />
       </Container>
     </Box>
   );
