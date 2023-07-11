6903bafa34b00cdd07d160740adba165f2abf826
Your Name <rghzaiel@ggn-group.com>
2023-07-11 15:15:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box, Container } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent>
+        App
+      </Container>
+    </Box>
+  );
+};
+
+export default App;
