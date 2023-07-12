4eefbd4b82abc88a949516699a902a23fc2fa118
Your Name <rghzaiel@ggn-group.com>
2023-07-12 10:27:19 +0100

    add keywords modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,54 @@
-import { Box, Container } from "@chakra-ui/react";
+import { useState } from "react";
+import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TextInput from "./components/TextInput";
-import { useState } from "react";
+import KeywordsModal from "./components/KeywordsModal";
 
 const App = () => {
-  const [keywords, setKeywords] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [keywords, setKeywords] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const extractKeywords = (text) => {
+  const extractKeywords = async (text) => {
     setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt:
+          "Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n" +
+          text +
+          "",
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_OPENAI_API_URL,
+        options
+      );
+      const json = await response.json();
+      setKeywords(json.choices[0].text.trim());
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
   };
 
   return (
@@ -20,6 +58,12 @@ const App = () => {
         <TextInput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
+      <KeywordsModal
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
     </Box>
   );
 };
